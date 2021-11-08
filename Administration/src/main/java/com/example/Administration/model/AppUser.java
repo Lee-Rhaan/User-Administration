4f21792ae763531ed3f621cd8c775b7b1b630b52
Lee-Rhaan <rhaanking@gmail.com>
2021-11-08 14:19:12 +0200

    Created user entity

--- Administration/src/main/java/com/example/Administration/model/AppUser.java
+++ Administration/src/main/java/com/example/Administration/model/AppUser.java
@@ -1,6 +1,29 @@
 package com.example.Administration.model;
 
+import com.example.Administration.enums.UserRoles;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Data
+@NoArgsConstructor
+@Entity
 public class AppUser implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    @Column(name = "role")
+    private UserRoles userRoles;
+
+    public AppUser(String username, String password, UserRoles userRoles)
+    {
+        this.username = username;
+        this.password = password;
+        this.userRoles = userRoles;
+    }
 }
