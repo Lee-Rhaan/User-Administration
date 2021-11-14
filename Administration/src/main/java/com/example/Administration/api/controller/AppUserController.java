f094f346120f65adbd57cfe5c9f06cfff9a5de34
Lee-Rhaan <rhaanking@gmail.com>
2021-11-14 09:24:13 +0200

    Documented class

--- Administration/src/main/java/com/example/Administration/api/controller/AppUserController.java
+++ Administration/src/main/java/com/example/Administration/api/controller/AppUserController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 /**
- *
+ * This is the controller class. It specifies the endpoints you'd have to use to get access to
+ * this server.
+ * The base request: "/api/v1/users/"
  */
 
 @RequiredArgsConstructor
@@ -20,6 +22,10 @@ public class AppUserController {
 
     private final AppUserService appUserService;
 
+    /**
+     * How to access this method: "/api/v1/users/all"
+     * @return list of all users stored in database
+     */
     @GetMapping("/all")
     public ResponseEntity<List<AppUser>> listAllUsers()
     {
@@ -27,6 +33,12 @@ public class AppUserController {
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
+    /**
+     * How to access this method: "/api/v1/users/create"
+     * create and saves new user in database.
+     * @param appUser object
+     * @return new saved user
+     */
     @PostMapping("/create")
     public ResponseEntity<AppUser> createUser(@RequestBody AppUser appUser)
     {
@@ -34,6 +46,12 @@ public class AppUserController {
         return new ResponseEntity<>(newAppUser, HttpStatus.CREATED);
     }
 
+    /**
+     * How to access this method: "/api/v1/users/update"
+     * If user exists in database, user's information gets updated
+     * @param appUser object
+     * @return updated user
+     */
     @PutMapping("/update")
     public ResponseEntity<AppUser> updateUser(@RequestBody AppUser appUser)
     {
@@ -41,6 +59,12 @@ public class AppUserController {
         return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
 
+    /**
+     * How to access this method: "/api/v1/users/find/{id}"
+     * Checks if user with matching id exists in database
+     * @param id Long
+     * @return user or exception
+     */
     @GetMapping("/find/{id}")
     public ResponseEntity<AppUser> findUserById(@PathVariable("id") Long id)
     {
@@ -48,6 +72,12 @@ public class AppUserController {
         return new ResponseEntity<>(appUser, HttpStatus.OK);
     }
 
+    /**
+     * How to access this method: "/api/v1/users/delete/{id}"
+     * If user with matching id exists in database, user gets removed.
+     * @param id Long
+     * @return Successful http status (200)
+     */
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteUserById(@PathVariable("id") Long id)
     {
