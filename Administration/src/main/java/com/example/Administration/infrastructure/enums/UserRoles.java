7cb42e84582afe38345d25b416bd43e45d03eb04
Lee-Rhaan <rhaanking@gmail.com>
2021-11-14 09:32:48 +0200

    Added Documentation

--- Administration/src/main/java/com/example/Administration/infrastructure/enums/UserRoles.java
+++ Administration/src/main/java/com/example/Administration/infrastructure/enums/UserRoles.java
@@ -1,15 +1,28 @@
 package com.example.Administration.infrastructure.enums;
 
+/**
+ * This enum class allows me to set the Roles of the users in this Administration application
+ * with predefined enum constants
+ */
+
 public enum UserRoles {
     ROLE_USER("ROLE_USER"), ROLE_ADMIN("ROLE_ADMIN");
 
     private final String userRole;
 
+    /**
+     * Initializing userRole variable with argument provided in parameters
+     *
+     * @param userRole String
+     */
     UserRoles(String userRole)
     {
         this.userRole = userRole;
     }
 
+    /**
+     * @return role of specified user
+     */
     public String getUserRole()
     {
         return userRole;
--- Administration/src/main/java/com/example/Administration/infrastructure/exception/AppUserNotFoundException.java
+++ Administration/src/main/java/com/example/Administration/infrastructure/exception/AppUserNotFoundException.java
@@ -2,6 +2,11 @@ package com.example.Administration.infrastructure.exception;
 
 public class AppUserNotFoundException extends RuntimeException {
 
+    /**
+     * This constructor will take the message provided when a new instance of this exception is created,
+     * and display it to the user.
+     * @param message String
+     */
     public AppUserNotFoundException(String message)
     {
         super(message);
